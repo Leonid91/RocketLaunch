b7fc36e6d3334875d049662297bb1cd60f0ef82c
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-05-12 17:51:27 +0200

    Add counter to change buffers

--- main.glsl
+++ main.glsl
@@ -2,8 +2,24 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = fragCoord/iResolution.xy;    
     vec4 col_a = texture(iChannel0, uv);
-    vec4 col_b = texture(iChannel1, uv); 
-    fragColor = mix (col_b, col_a, col_a.a);
+    vec4 col_b = texture(iChannel1, uv);
+    vec4 col_c = texture(iChannel2, uv);
+    
+    float counter = 0.0;
+    counter = iTime;
+    while(counter < 5.){
+        fragColor = mix (col_c, col_a, col_a.a);
+    }
+    while(counter < 10. && counter > 5.){
+        fragColor = mix (col_b, col_a, col_a.a);
+    }
+    while(counter < 15. && counter > 10.){
+        counter = 0.;
+    }
+    
+    //Use this for debug
+    //fragColor = mix (col_b, col_a, col_a.a);
+    //fragColor = mix (col_c, col_a, col_a.a);
     //fragColor = mix (col_b, col_a, 1.);
-    //fragColor = col_a;
+    //fragColor = col_c;
 }
